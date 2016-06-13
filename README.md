15b86eabf2ef82b70d887282b1235202ff80a8e1
Christophe FEIJOO <cfeijoo@e2r13p12.42.fr>
2016-06-13 16:20:02 +0200

    Working version

---
+++ README.md
@@ -0,0 +1,34 @@
+42 Homebrew Fix
+===============
+
+This script installs Homebrew properly in your home on your 42 session.
+
+It fixes the `flock` issue due to NFS mount of your home directory.
+
+
+Install
+-------
+
+Run this command from your terminal:
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/kube/42homebrew_fix/master/install.sh)"
+```
+
+How it works
+------------
+This script removes your current Homebrew installation in your home, and reinstalls it properly from the Homebrew Github repo.
+
+Then it simply creates a `.brew_fix.zsh` script in your home directory, and modifies your `.zshrc` to source the script.
+
+It is simple to remove, and you can re-run it multiple times without duplication.
+
+
+Uninstall
+---------
+Simply remove these lines from your .zshrc
+```
+# Load Homebrew Fix script
+source \$HOME/.brew_fix.zsh
+```
+
+And delete `.brew_fix.zsh` in your home directory.
---
+++ install.sh
@@ -0,0 +1,39 @@
+# # Delete and reinstall Homebrew from Github repo
+# rm -rf $HOME/.brew
+# git clone git@github.com:Homebrew/homebrew $HOME/.brew
+
+# Create .brew_fix script in home directory
+cat > $HOME/.brew_fix.zsh <<EOL
+# HOMEBREW CONFIG
+
+# Add brew to path
+export PATH=\$HOME/.brew/bin:\$PATH
+
+# Set Homebrew temporary folders
+export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
+export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
+export HOMEBREW_LOCKS=/tmp/\$USER/Homebrew/Locks
+
+mkdir -p \$HOMEBREW_CACHE
+mkdir -p \$HOMEBREW_TEMP
+
+# Create symlink for Locks folder
+rm -rf \$HOME/.brew/Library/Locks
+mkdir -p \$HOMEBREW_LOCKS
+ln -s /tmp/\$USER/Homebrew/Locks \$HOME/.brew/Library/Locks
+
+EOL
+
+# Add .brew_fix sourcing in your .zshrc
+if ! grep -q "# Load Homebrew Fix script" "$HOME/.zshrc"; then
+
+cat >> $HOME/.zshrc <<EOL
+
+# Load Homebrew Fix script
+source \$HOME/.brew_fix.zsh
+EOL
+
+fi
+
+source $HOME/.zshrc
+brew update
