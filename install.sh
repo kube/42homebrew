f7d277a9a2c1a222500691bfd6ec7a60f9f5a3a0
Christophe FEIJOO <cfeijoo@e2r13p12.42.fr>
2016-09-22 15:24:30 +0200

    Update Locks folder

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ mkdir -p \$HOMEBREW_TEMP
 # Create symlink for Locks folder
 rm -rf \$HOME/.brew/Library/Locks
 mkdir -p \$HOMEBREW_LOCKS
-ln -s /tmp/\$USER/Homebrew/Locks \$HOME/.brew/Library/Locks
+ln -s /tmp/\$USER/Homebrew/Locks \$HOME/.brew/var/library/locks
 
 EOL
 
