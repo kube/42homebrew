b89ad576e82343bef3cc1a2236cce96657c00e01
Chris Feijoo <kube@users.noreply.github.com>
2016-12-20 11:23:22 +0100

    Tweak syntax and add comment

--- install.sh
+++ install.sh
@@ -26,6 +26,8 @@ export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
 mkdir -p \$HOMEBREW_CACHE
 mkdir -p \$HOMEBREW_TEMP
 
+# If NFS session
+# Symlink Locks folder in /tmp
 if df -T autofs,nfs \$HOME 1>/dev/null
 then
   HOMEBREW_LOCKS_TARGET=/tmp/\$USER/Homebrew/Locks
@@ -36,7 +38,8 @@ then
 
   # Symlink to Locks target folders
   # If not already a symlink
-  if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]; then
+  if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
+  then
      echo "Creating symlink for Locks folder"
      rm -rf \$HOMEBREW_LOCKS_FOLDER
      ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
