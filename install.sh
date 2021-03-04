9f8475d5023a58a316f319f1aac724bcbe3ce443
Amine TAHIRI <31249214+atah1r1@users.noreply.github.com>
2021-03-04 10:22:18 +0100

    (update): install brew and minikube on goinfre

--- install.sh
+++ install.sh
@@ -1,41 +1,33 @@
-
-      #########.
-     ########",#:
-   #########',##".
-  ##'##'## .##',##.
-   ## ## ## # ##",#.
-    ## ## ## ## ##'
-     ## ## ## :##
-      ## ## ##."
-
 # Delete and reinstall Homebrew from Github repo
-rm -rf $HOME/.brew
+rm -rf $HOME/.brew $HOME/goinfre/.brew
+mkdir /goinfre/$(whoami)/.brew
+ln -s /goinfre/$(whoami)/.brew $HOME/.brew
 git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
+
+# Create Extra Symlinks for Minikube and Docker
+rm -rf $HOME/.minikube $HOME/goinfre/.minikube $HOME/.docker $HOME/goinfre/.docker
+mkdir /goinfre/$(whoami)/.minikube /goinfre/$(whoami)/.docker
+ln -s /goinfre/$(whoami)/.minikube $HOME/.minikube
+ln -s /goinfre/$(whoami)/.docker $HOME/.docker
 
 # Create .brewconfig script in home directory 
 cat > $HOME/.brewconfig.zsh <<EOL
 # HOMEBREW CONFIG
-
 # Add brew to path
 export PATH=\$HOME/.brew/bin:\$PATH
-
 # Set Homebrew temporary folders
 export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
 export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
-
 mkdir -p \$HOMEBREW_CACHE
 mkdir -p \$HOMEBREW_TEMP
-
 # If NFS session
 # Symlink Locks folder in /tmp
 if df -T autofs,nfs \$HOME 1>/dev/null
 then
   HOMEBREW_LOCKS_TARGET=/tmp/\$USER/Homebrew/Locks
   HOMEBREW_LOCKS_FOLDER=\$HOME/.brew/var/homebrew
-
   mkdir -p \$HOMEBREW_LOCKS_TARGET
   mkdir -p \$HOMEBREW_LOCKS_FOLDER
-
   # Symlink to Locks target folders
   # If not already a symlink
   if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
@@ -51,7 +43,6 @@ EOL
 if ! grep -q "# Load Homebrew config script" $HOME/.zshrc
 then
 cat >> $HOME/.zshrc <<EOL
-
 # Load Homebrew config script
 source \$HOME/.brewconfig.zsh
 EOL
@@ -60,5 +51,7 @@ fi
 source $HOME/.brewconfig.zsh
 rehash
 brew update
+sleep 3
+brew install minikube
 
 echo "\nPlease open a new shell to finish installation"
